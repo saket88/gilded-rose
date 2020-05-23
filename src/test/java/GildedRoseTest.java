a80ace2b6a10c69b5b75a10bb4d0233daf66a92a
Saket Puranik <saket@backbase.com>
2020-05-23 11:57:55 +0200

    Enabled code coverage and parametrized the test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,16 +1,29 @@
 import com.gildedrose.GildedRose;
 import com.gildedrose.Item;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+
+    @DisplayName("Should able to update quality")
+    @ParameterizedTest(name = "{index} => name={0}, sellIn={1}, quality={2}")
+    @CsvSource({
+            "foo, 0, 0"
+    })
+    void testUpdateQuality( String name, int sellIn, int quality ) {
+        GildedRose app = updateQuality( name, sellIn, quality );
+        assertEquals( name, app.items[0].name);
+    }
+
+    private GildedRose updateQuality( String name, int sellIn, int quality ) {
+        Item[] items = new Item[] { new Item( name,  sellIn, quality ) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        return app;
     }
 }
